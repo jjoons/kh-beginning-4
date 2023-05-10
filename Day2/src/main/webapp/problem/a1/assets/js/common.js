7821933e3d8a6b96adbe80a6dfc8e8a8e435cc06
JJoonS <jjoons@jjoons.com>
2023-05-10 18:01:06 +0900

    0510 2

---
+++ Day2/src/main/webapp/problem/a1/assets/js/common.js
@@ -0,0 +1,13 @@
+'use strict'
+
+$(function ($) {
+  $('header .__main_header .__menu_button > button').on('click', function (e) {
+    const a = $('.__mobile_menu')
+    a.toggleClass('on')
+  })
+
+  $('header .__mobile_menu >.__mobile_menu_close_button').on('click', function () {
+    const a = $('.__mobile_menu')
+    a.removeClass('on')
+  })
+})
