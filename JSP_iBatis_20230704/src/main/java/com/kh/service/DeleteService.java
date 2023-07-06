c3fe7b2190a01d73a9e962c430f8f27556c88487
JJoonS <jjoons@jjoons.com>
2023-07-06 11:23:31 +0900

    0706 1

--- JSP_iBatis_20230704/src/main/java/com/kh/service/DeleteService.java
+++ JSP_iBatis_20230704/src/main/java/com/kh/service/DeleteService.java
@@ -1,16 +1,30 @@
 package com.kh.service;
 
+import java.sql.SQLException;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.kh.dao.GuestbookDAO;
+import com.kh.ibatis.MyAppSqlConfig;
+
 public class DeleteService {
-  //singleton 클래스 디자인 패턴은 한 순간에 단 한 개의 객체만 필요할 경우 사용하는 디자인 패턴이다.
-  //singleton 패턴 코딩 방식
-  //1. 자기 자신(현재 클래스)의 객체를 기본 생성자를 사용해서 정적 필드로 선언한다.
   private static DeleteService instance = new DeleteService();
 
-  //2. 클래스 외부에서 객체를 생성할 수 없도록 기본 생성자의 접근 권한을 private으로 변경한다.
   private DeleteService() {}
 
-  //3. 자신의 객체를 리턴시키는 정적 메소드를 만든다.
   public static DeleteService getInstance() {
     return instance;
+  }
+
+  // deleteOK.jsp에서 호출되는 삭제할 글 번호를 넘겨받고 mapper를 얻어온 후
+  // 게스트북DAO 클래스의 글 1건을 삭제하는 DELETE SQL 명령을 실행하는 메소드를 호출하는 메소드
+  public void delete(int idx) {
+    System.out.println("DeleteService().delete()");
+
+    SqlMapClient mapper = MyAppSqlConfig.getSqlMapInstance();
+
+    try {
+      GuestbookDAO.getInstance().delete(mapper, idx);
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
   }
 }
--- JSP_iBatis_20230704/src/main/java/com/kh/service/UpdateService.java
+++ JSP_iBatis_20230704/src/main/java/com/kh/service/UpdateService.java
@@ -1,5 +1,36 @@
 package com.kh.service;
 
-public class UpdateService {
+import java.sql.SQLException;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.kh.dao.GuestbookDAO;
+import com.kh.ibatis.MyAppSqlConfig;
+import com.kh.vo.GuestbookVO;
 
+public class UpdateService {
+  private static UpdateService instance = null;
+
+  private UpdateService() {}
+
+  public static UpdateService getInstance() {
+    if (instance == null) {
+      instance = new UpdateService();
+    }
+
+    return instance;
+  }
+
+  // updateOK.jsp에서 호출되는 수정할 정보가 저장된 객체를 넘겨받고
+  // mapper를 얻은 후 GuestbookDAO 클래스에서 글 1건을 수정하는
+  // UPDATE SQL 명령을 실행하는 메소드를 호출하는 메소드
+  public void update(GuestbookVO vo) {
+    System.out.println("DeleteService().delete()");
+
+    SqlMapClient mapper = MyAppSqlConfig.getSqlMapInstance();
+
+    try {
+      GuestbookDAO.getInstance().update(mapper, vo);
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+  }
 }
